458b56afb6a29db41b422bbe89591b70ee0cd1a7
komalsorte <ks7229@rit.edu>
2020-02-01 15:30:32 -0500

    v4

--- Numbers/Maximum_69_Number.py
+++ Numbers/Maximum_69_Number.py
@@ -4,16 +4,17 @@ class Solution(object):
         :type num: int
         :rtype: int
         """
-        if num!= 9999:
-            num_s = str(num)
-            for digit in range(len(num_s)):
-                if num_s[digit] == '6':
-                    first_part = num_s[:digit]
-                    second_part = num_s[digit + 1: ]
-                    num_s = first_part + '9' + second_part
-                    num = int(num_s)
-                    return num
-        else:
-            return num
+
+        num_s = str(num)
+        for digit in range(len(num_s)):
+            if num_s[digit] == '6':
+                first_part = num_s[:digit]
+                second_part = num_s[digit + 1:]
+                num_s = first_part + '9' + second_part
+                num = int(num_s)
+                break
+        return num
+
+
 if __name__ == '__main__':
-    Solution().maximum69Number(9669)
+    Solution().maximum69Number(69)
