e0333a533911b3eac64c095543afd6990a9e548d
komalsorte <ks7229@rit.edu>
2020-02-01 15:37:30 -0500

    v4

---
+++ Numbers/FindNumbers.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def findNumbers(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        # convert integer to string
+        count = 0
+        for i in range(len(nums)):
+            if len(str(nums[i]))%2 ==0 :
+                count = count + 1
+
+        print(count)
+        return count
+if __name__ == '__main__':
+    Solution().findNumbers([12, 345, 2, 6, 7896])
