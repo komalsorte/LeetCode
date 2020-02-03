b1f746b42ff2fedcaa1236615e087774ab883003
komalsorte <ks7229@rit.edu>
2020-02-02 21:28:46 -0500

    Final

--- Prefix_String.py
+++ Prefix_String.py
@@ -1,21 +1,28 @@
+"""
+EBAY Question 2
+"""
 def prefixString(a,b):
-    items = dict()
+    all_possible_prefix = dict()
     string_s = ''
-    new_string = ''
     flag = False
     for j in range(len(a)):
         if string_s == '':
             string_s = a[j]
+            all_possible_prefix[string_s] = string_s
         else:
             string_s = string_s + a[j]
-            items[string_s] = string_s
-    for ele in b:
-        new_string += ele
+            all_possible_prefix[string_s] = string_s
+            print(string_s)
+    print(all_possible_prefix)
 
-    if new_string in items.keys():
-        flag = True
+    for ele in b:
+        if ele in all_possible_prefix.keys():
+            flag = True
+        else:
+            return False
+    print(flag)
     return flag
 if __name__ == '__main__':
     a = ["One", "Two","Three"]
-    b= ["Two"]
+    b= ["OneTwo" , "One"]
     prefixString(a,b)
