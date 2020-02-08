045bbb3565b25083d54aa61ca8de68bf832ad8e3
komalsorte <ks7229@rit.edu>
2020-02-08 13:39:20 -0500

    Feb 8

--- Arrays/349_IntersectionOfTwoArrays.py
+++ Arrays/349_IntersectionOfTwoArrays.py
@@ -2,13 +2,16 @@
 Easy
 """
 from typing import List
+
+
 class Solution:
-    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+    def intersection(self, nums1, nums2):
         dict_int_1 = dict()
         dict_int_2 = dict()
         index_1 = 0
         index_2 = 0
-        while index_1 < len(nums1) and index_2 < len(nums2):
+        flag = True
+        while flag == True:
 
             if index_1 != len(nums1):
                 if nums1[index_1] in dict_int_1:
@@ -16,6 +19,7 @@ class Solution:
                 else:
                     dict_int_1[nums1[index_1]] = 1
                 index_1 += 1
+
             if index_2 != len(nums2):
                 if nums2[index_2] in dict_int_2:
                     dict_int_2[nums2[index_2]] += 1
@@ -23,6 +27,8 @@ class Solution:
                     dict_int_2[nums2[index_2]] = 1
                 index_2 += 1
 
+            if index_1 == len(nums1) and index_2 == len(nums2):
+                flag = False
 
         list_intersection = []
         for item, frequency in dict_int_1.items():
@@ -30,7 +36,8 @@ class Solution:
                 list_intersection.append(item)
         return list_intersection
 
+
 if __name__ == '__main__':
-    nums1 = [1,2,2,1],
-    nums2 = [2,2]
+    nums1 = [3, 1, 2]
+    nums2 = [1]
     print(Solution().intersection(nums1, nums2))
