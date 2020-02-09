133b49220eea749c7d2e64401e566fa2e886ef0f
komalsorte <ks7229@rit.edu>
2020-02-09 12:26:49 -0500

    Feb 8

---
+++ Arrays/961_N-RepeatedElementinSize2NArray.py
@@ -0,0 +1,21 @@
+"""
+Easy
+"""
+
+
+class Solution:
+    def repeatedNTimes(self, A):
+
+        dict_A = dict()
+        element = ''
+        for index_1 in range(len(A)):
+            if A[index_1] in dict_A:
+                dict_A[A[index_1]] += 1
+                element = A[index_1]
+                break
+            else:
+                dict_A[A[index_1]] = 1
+        return element
+
+if __name__ == '__main__':
+    print(Solution().repeatedNTimes([1, 2, 3, 3]))
