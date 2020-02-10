69438ee029add2453c75dafdfa4431a663ee26d9
komalsorte <ks7229@rit.edu>
2020-02-09 19:05:18 -0500

    Feb 9

---
+++ Arrays/442_FindAllDuplicates.py
@@ -0,0 +1,24 @@
+"""
+Medium
+"""
+
+
+class Solution:
+    def findDuplicates(self, nums):
+        dict_nums = dict()
+        dict_nums_res = dict()
+        for num in nums:
+            if num in dict_nums:
+                dict_nums[num] += 1
+                dict_nums_res[num] = num
+            else:
+                dict_nums[num] = 1
+        return list(dict_nums_res)
+
+
+
+
+
+if __name__ == '__main__':
+    nums = [4,3,2,7,8,2,3,1]
+    print(Solution().findDuplicates(nums))
