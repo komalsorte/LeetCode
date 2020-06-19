5bf10467d91e722d10a05a2563b3e3c44ff2cb7b
komal sorte <ks7229@g.rit.edu>
2020-06-19 16:44:23 -0400

    Amazon - Card

--- Amazon/48_RotateImage.py
+++ Amazon/48_RotateImage.py
@@ -24,6 +24,7 @@ class Solution:
             matrix[r].reverse()
         return matrix
 
+
 if __name__ == '__main__':
     matrix = [
         [1, 2, 3],
@@ -31,9 +32,9 @@ if __name__ == '__main__':
         [7, 8, 9]
     ]
     matrix = [
-        [5,  1, 9, 11],
-        [2,  4, 8, 10],
+        [5, 1, 9, 11],
+        [2, 4, 8, 10],
         [13, 3, 6, 7],
-        [15, 14,12,16]
+        [15, 14, 12, 16]
     ]
     print(Solution().rotate(matrix))
