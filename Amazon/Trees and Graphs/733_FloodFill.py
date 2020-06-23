80fd4a5ad81df2a73f1db58d157678ba0aa0bd47
komal sorte <ks7229@g.rit.edu>
2020-06-23 12:16:19 -0400

    Amazon - Card

---
+++ Amazon/Trees and Graphs/733_FloodFill.py
@@ -0,0 +1,71 @@
+__author__ = "Komal Atul Sorte"
+"""
+An image is represented by a 2-D array of integers, each integer representing the pixel value of the image (from 0 to 65535).
+
+Given a coordinate (sr, sc) representing the starting pixel (row and column) of the flood fill, and a pixel value newColor, "flood fill" the image.
+
+To perform a "flood fill", consider the starting pixel, plus any pixels connected 4-directionally to the starting pixel of the same color as the starting pixel, plus any pixels connected 4-directionally to those pixels (also with the same color as the starting pixel), and so on. Replace the color of all of the aforementioned pixels with the newColor.
+
+At the end, return the modified image.
+
+Example 1:
+Input: 
+image = [[1,1,1],[1,1,0],[1,0,1]]
+sr = 1, sc = 1, newColor = 2
+Output: [[2,2,2],[2,2,0],[2,0,1]]
+Explanation: 
+From the center of the image (with position (sr, sc) = (1, 1)), all pixels connected 
+by a path of the same color as the starting pixel are colored with the new color.
+Note the bottom corner is not colored 2, because it is not 4-directionally connected
+to the starting pixel.
+Note:
+
+The length of image and image[0] will be in the range [1, 50].
+The given starting pixel will satisfy 0 <= sr < image.length and 0 <= sc < image[0].length.
+The value of each color in image[i][j] and newColor will be an integer in [0, 65535].
+"""
+
+
+class Solution:
+    def floodFill(self, image, sr, sc, newColor):
+        starting_index = image[sr][sc]
+        temp_newColor = newColor
+        same_flag = False
+        if starting_index == newColor:
+            newColor = "#"
+            same_flag = True
+        print(starting_index)
+        image = self.dfs(image, sr, sc, newColor, starting_index)
+        if same_flag:
+            for r in range(len(image)):
+                for c in range(len(image[r])):
+                    if image[r][c] == newColor:
+                        image[r][c] = temp_newColor
+
+        return image
+
+    def dfs(self, image, sr, sc, newColor, starting_index):
+        if sr < 0 or sc < 0 or sr >= len(image) or sc >= len(image[sr]) or image[sr][sc] != starting_index:
+            return image
+        image[sr][sc] = newColor
+        image = self.dfs(image, sr + 1, sc, newColor, starting_index)
+        image = self.dfs(image, sr - 1, sc, newColor, starting_index)
+        image = self.dfs(image, sr, sc + 1, newColor, starting_index)
+        image = self.dfs(image, sr, sc - 1, newColor, starting_index)
+        return image
+
+
+if __name__ == '__main__':
+    image = [[1, 1, 1],
+             [1, 1, 0],
+             [1, 0, 1]]
+    sr = 1
+    sc = 1
+    newColor = 2
+    #
+    # image = [[0, 0, 0], [0, 1, 1]]
+    # sr = 1
+    # sc = 1
+    # newColor = 1
+
+    print(Solution().floodFill(image, sr, sc, newColor))
