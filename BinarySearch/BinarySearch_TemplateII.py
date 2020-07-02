5ad1d22e1f89cdad78d8859e7e26df06771f559b
komal.sorte <komal.sorte@siemens.com>
2020-07-01 21:45:50 -0400

    Binary Search Template II

---
+++ BinarySearch/BinarySearch_TemplateII.py
@@ -0,0 +1,33 @@
+# LeetCode
+def binarySearch(nums, target):
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: int
+    """
+    if len(nums) == 0:
+        return -1
+
+    left, right = 0, len(nums)
+    while left < right:
+        mid = (left + right) // 2
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            left = mid + 1
+        else:
+            right = mid
+
+    # Post-processing:
+    # End Condition: left == right
+    if left != len(nums) and nums[left] == target:
+        return left
+    return -1
+
+if __name__ == '__main__':
+    nums = [-1, 0, 3, 5, 9, 12]
+    target = -2
+
+    # nums = [5]
+    # target = 5
+    print(binarySearch(nums, target))
