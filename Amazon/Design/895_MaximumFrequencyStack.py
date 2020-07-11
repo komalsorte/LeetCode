953908047a948e9ac89a3870fc2cbd11871c55d0
komal sorte <ks7229@g.rit.edu>
2020-07-10 20:41:04 -0400

    895

---
+++ Amazon/Design/895_MaximumFrequencyStack.py
@@ -0,0 +1,73 @@
+__author__ = "Komal Atul Sorte"
+
+"""
+Implement FreqStack, a class which simulates the operation of a stack-like data structure.
+
+FreqStack has two functions:
+
+push(int x), which pushes an integer x onto the stack.
+pop(), which removes and returns the most frequent element in the stack.
+If there is a tie for most frequent element, the element closest to the top of the stack is removed and returned.
+ 
+
+Example 1:
+
+Input: 
+["FreqStack","push","push","push","push","push","push","pop","pop","pop","pop"],
+[[],[5],[7],[5],[7],[4],[5],[],[],[],[]]
+Output: [null,null,null,null,null,null,null,5,7,5,4]
+Explanation:
+After making six .push operations, the stack is [5,7,5,7,4,5] from bottom to top.  Then:
+
+pop() -> returns 5, as 5 is the most frequent.
+The stack becomes [5,7,5,7,4].
+
+pop() -> returns 7, as 5 and 7 is the most frequent, but 7 is closest to the top.
+The stack becomes [5,7,5,4].
+
+pop() -> returns 5.
+The stack becomes [5,7,4].
+
+pop() -> returns 4.
+The stack becomes [5,7].
+
+"""
+
+import collections
+
+
+class FreqStack(object):
+
+    def __init__(self):
+        self.freq = collections.Counter()
+        self.group = collections.defaultdict(list)
+        self.maxfreq = 0
+
+    def push(self, x):
+        f = self.freq[x] + 1
+        self.freq[x] = f
+        if f > self.maxfreq:
+            self.maxfreq = f
+        self.group[f].append(x)
+
+    def pop(self):
+        x = self.group[self.maxfreq].pop()
+        self.freq[x] -= 1
+        if not self.group[self.maxfreq]:
+            self.maxfreq -= 1
+
+        return x
+
+
+# Your FreqStack object will be instantiated and called as such:
+obj = FreqStack()
+obj.push(5)
+obj.push(7)
+obj.push(5)
+obj.push(7)
+obj.push(4)
+obj.push(5)
+print(obj.pop())
+print(obj.pop())
+print(obj.pop())
+print(obj.pop())
