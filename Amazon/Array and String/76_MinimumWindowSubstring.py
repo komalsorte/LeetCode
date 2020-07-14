7345d04093eced00d2d3fc046649cd18a48035b9
komal sorte <ks7229@g.rit.edu>
2020-07-13 20:52:20 -0400

    76

--- Amazon/Array and String/76_MinimumWindowSubstring.py
+++ Amazon/Array and String/76_MinimumWindowSubstring.py
@@ -10,18 +10,26 @@ class Solution:
         end = 0
         output_substring = list()
         T_temp = list(T)
+        output = ''
+        flag = False
         while start < len(S) and end < len(S):
             if S[end] in T_temp and len(T) == len(T_temp):
                 T_temp.remove(S[end])
+                flag = True
                 start = end
             elif S[end] in T_temp:
                 T_temp.remove(S[end])
+            if flag:
+                output = output + S[end]
             end += 1
+
             if len(T_temp) == 0:
-                output_substring.append(S[start:end])
+                output_substring.append(output)
+                output = ''
                 start = start + 1
                 end = start
                 T_temp = list(T)
+                flag = False
         if len(output_substring) == 0:
             return ""
         else:
@@ -31,6 +39,8 @@ class Solution:
 if __name__ == '__main__':
     S = "ADOBECODEBANC"
     T = "ABC"
-    S = "aa"
-    T = "aaa"
+    # S = "aa"
+    # T = "aaa"
+    S = "ab"
+    T = "b"
     print(Solution().minimumWindowSubstring(S, T))
