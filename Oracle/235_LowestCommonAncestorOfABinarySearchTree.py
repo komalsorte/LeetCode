92067b0333696e9948fc1d521b2e0bc485076253
Komal Sorte <ks7229@g.rit.edu>
2020-10-20 23:08:06 -0400

    235

---
+++ Oracle/235_LowestCommonAncestorOfABinarySearchTree.py
@@ -0,0 +1,73 @@
+"""
+LeetCode - Easy
+"""
+"""
+Given a binary search tree (BST), find the lowest common ancestor (LCA) of two given nodes in the BST.
+
+According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+
+ 
+
+Example 1:
+
+
+Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+Output: 6
+Explanation: The LCA of nodes 2 and 8 is 6.
+Example 2:
+
+
+Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
+Output: 2
+Explanation: The LCA of nodes 2 and 4 is 2, since a node can be a descendant of itself according to the LCA definition.
+Example 3:
+
+Input: root = [2,1], p = 2, q = 1
+Output: 2
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [2, 105].
+-109 <= Node.val <= 109
+All Node.val are unique.
+p != q
+p and q will exist in the BST.
+
+"""
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x, left=None, right=None):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if root is None:
+            return
+        if root.val == p.val or root.val == q.val:
+            return root
+        if p.val < root.val and q.val < root.val:
+            node = self.lowestCommonAncestor(root.left, p, q)
+        elif p.val > root.val and q.val > root.val:
+            node = self.lowestCommonAncestor(root.right, p, q)
+        else:
+            return root
+        return node
+
+if __name__ == '__main__':
+    root = TreeNode(6)
+    left = TreeNode(2, TreeNode(0), TreeNode(4, TreeNode(3), TreeNode(5)))
+    right = TreeNode(8, TreeNode(7), TreeNode(9))
+    root.left = left
+    root.right = right
+
+    p = left
+    q = right
+    ans = Solution().lowestCommonAncestor(root, p, q)
+    print(ans.val)
+
+
