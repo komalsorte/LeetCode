100bab6fc802a80587f10d4e9f1ef3b7b74fb25d
Komal Sorte <ks7229@g.rit.edu>
2020-10-31 13:35:08 -0400

    -

---
+++ ChallengeSeptember2020/LargestTimeForGivenDigits.py
@@ -0,0 +1,22 @@
+"""
+LeetCode - Medium
+"""
+
+
+class Solution:
+    def largestTimeFromDigits(self, arr):
+        hour = 0
+        if 2 in arr:
+            hour = 2
+            if
+        elif 1 in arr:
+            hour = 1
+        elif 0 in arr:
+            hour = 0
+        else:
+            return ""
+
+
+if __name__ == '__main__':
+    arr = [1, 2, 3, 4]
+    print(Solution().largestTimeFromDigits(arr))
