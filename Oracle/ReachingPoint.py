96c602e9493832bd57d5b1181833e898bfd5ee6e
Komal Sorte <ks7229@g.rit.edu>
2020-10-31 13:35:13 -0400

    -

---
+++ Oracle/ReachingPoint.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def reachingPoints(self, sx, sy, tx, ty):
+        while tx >= sx and ty >= sy:
+            if tx == ty:
+                break
+            elif tx > ty:
+                if ty > sy:
+                    tx %= ty
+                else:
+                    return (tx - sx) % ty == 0
+            else:
+                if tx > sx:
+                    ty %= tx
+                else:
+                    return (ty - sy) % tx == 0
+
+        return tx == sx and ty == sy
+
+
+print(Solution().reachingPoints(sx=1, sy=1, tx=3, ty=5))
