40fef9db0f635aa72e8b370665b5b5b191b2e913
Komal Sorte <ks7229@g.rit.edu>
2020-10-31 13:34:13 -0400

    428

---
+++ NAryTree/428_SerializeAndDeserializeNAryTree.py
@@ -0,0 +1,92 @@
+"""
+LeetCode - Hard
+"""
+import ast
+import json
+
+"""
+Serialization is the process of converting a data structure or object into a sequence of bits so that it can be stored in a file or memory buffer, or transmitted across a network connection link to be reconstructed later in the same or another computer environment.
+
+Design an algorithm to serialize and deserialize an N-ary tree. An N-ary tree is a rooted tree in which each node has no more than N children. There is no restriction on how your serialization/deserialization algorithm should work. You just need to ensure that an N-ary tree can be serialized to a string and this string can be deserialized to the original tree structure.
+
+For example, you may serialize the following 3-ary tree
+
+
+
+as [1 [3[5 6] 2 4]]. Note that this is just an example, you do not necessarily need to follow this format.
+
+Or you can follow LeetCode's level order traversal serialization format, where each group of children is separated by the null value.
+
+
+
+For example, the above tree may be serialized as [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,null,13,null,null,14].
+
+You do not necessarily need to follow the above suggested formats, there are many more different formats that work so please be creative and come up with different approaches yourself.
+
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 104].
+0 <= Node.val <= 104
+The height of the n-ary tree is less than or equal to 1000
+Do not use class member/global/static variables to store states. Your encode and decode algorithms should be stateless.
+"""
+
+
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+
+
+class Codec:
+    def __init__(self):
+        self.serializeTable = dict()
+        self.deserializeTree = None
+
+    def serialize(self, root: 'Node') -> str:
+        """Encodes a tree to a single string.
+
+        :type root: Node
+        :rtype: str
+        """
+        if root is None:
+            return None
+        if root.val not in self.serializeTable:
+            self.serializeTable[root.val] = []
+        if root.children:
+            for child in root.children:
+                self.serialize(child)
+                self.serializeTable[root.val].append(child.val)
+        return str(self.serializeTable)
+
+    def deserialize(self, data: str) -> 'Node':
+        """Decodes your encoded data to tree.
+
+        :type data: str
+        :rtype: Node
+        """
+        data = eval(data)
+        print(type(data))
+        print(data)
+
+        self.deserializeHelper(data)
+        return self.deserializeTree
+
+    def deserializeHelper(self, data):
+        if self.deserializeTree is None:
+
+
+
+if __name__ == '__main__':
+    # Your Codec object will be instantiated and called as such:
+    NodeA = Node(3, [Node(5, [Node(50)]), Node(6)])
+    NodeB = Node(2)
+    NodeC = Node(4, [Node(15), Node(16)])
+    root = Node(1, [NodeA, NodeB, NodeC])
+
+    codec = Codec()
+    print(codec.serialize(root))
+    codec.deserialize(codec.serialize(root))
